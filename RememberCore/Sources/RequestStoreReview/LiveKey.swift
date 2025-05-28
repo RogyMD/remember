7715840e54f463172911734a7d41d0099d28385a
Igor Bidiniuc <igorbidiniuc@gmail.com>
2025-05-28 18:32:02 +0100

    fix request app review

--- RememberCore/Sources/RequestStoreReview/LiveKey.swift
+++ RememberCore/Sources/RequestStoreReview/LiveKey.swift
@@ -3,12 +3,29 @@ import StoreKit
 
 extension RequestStoreReview: DependencyKey {
   public static let liveValue = Self {
-#if !DEBUG && canImport(UIKit.UIApplication)
+#if canImport(UIKit.UIApplication)
     guard let scene = await UIApplication.shared.keyWindowScene else {
       assertionFailure("Can't find key window.")
       return
     }
-    SKStoreReviewController.requestReview(in: scene)
+#if !DEBUG
+    await AppStore.requestReview(in: scene)
+#else
+    _ = scene
+#endif
 #endif
   }
 }
+
+#if canImport(UIKit.UIApplication)
+extension UIApplication {
+  public var keyWindowScene: UIWindowScene? {
+    connectedScenes
+      .compactMap({ $0 as? UIWindowScene })
+      .first(where: { scene in scene.windows.contains(where: \.isKeyWindow) })
+  }
+  public var topViewController: UIViewController? {
+    keyWindowScene?.keyWindow?.rootViewController
+  }
+}
+#endif
