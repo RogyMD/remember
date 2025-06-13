b36bc49524e9cc79b8b62a994a26757a4e932540
Igor Bidiniuc <igorbidiniuc@gmail.com>
2025-06-13 23:51:50 +0100

    upd: improved search

--- RememberCore/Sources/DatabaseClient/FetchDescriptors.swift
+++ RememberCore/Sources/DatabaseClient/FetchDescriptors.swift
@@ -27,19 +27,62 @@ extension FetchDescriptor where T == MemoryModel {
   }
   
   static func search(_ query: String) -> Self {
-    descriptor(
-      predicate: #Predicate { memory in
-        memory.tags.contains(where: { item in
-          item.label.localizedStandardContains(query)
+    let regex = try! NSRegularExpression(pattern: "\\p{L}+", options: [])
+    let range = NSRange(query.startIndex..<query.endIndex, in: query)
+    let words: [String] = regex.matches(in: query, options: [], range: range).compactMap {
+      guard let range = Range($0.range, in: query) else {
+        return nil
+      }
+      return String(query[range]).lowercased()
+    }
+    let perWordPredicates: [Predicate<MemoryModel>] = words.map { word in
+      #Predicate<MemoryModel> { memory in
+        memory.tags.contains(where: { tag in
+          tag.label.localizedStandardContains(word)
         }) ||
         memory.items.contains(where: { item in
-          item.name.localizedStandardContains(query)
+          item.name.localizedStandardContains(word)
         }) ||
-        memory.notes.localizedStandardContains(query)
-      },
+        memory.notes.localizedStandardContains(word)
+      }
+    }
+    // searching if the query is included in any of the properties
+    let basePredicate: Predicate<MemoryModel> = #Predicate { memory in
+      memory.tags.contains(where: { item in
+        item.label.localizedStandardContains(query)
+      }) ||
+      memory.items.contains(where: { item in
+        item.name.localizedStandardContains(query)
+      }) ||
+      memory.notes.localizedStandardContains(query)
+    }
+    // Now combine with AND (all words must match somewhere)
+    let searchPredicate: Predicate<MemoryModel>? = perWordPredicates.reduce(nil) { acc, next in
+      if let acc {
+        #Predicate { memory in
+          acc.evaluate(memory) && next.evaluate(memory)
+        } as Predicate<MemoryModel>
+      } else {
+        next
+      }
+    }
+    // combine the sum of results
+    let combinedPredicate: Predicate<MemoryModel> = {
+      if let searchPredicate {
+        return #Predicate<MemoryModel> { memory in
+          basePredicate.evaluate(memory) || searchPredicate.evaluate(memory)
+        }
+      } else {
+        return basePredicate
+      }
+    }()
+    
+    return descriptor(
+      predicate: combinedPredicate,
       sortBy: [
         SortDescriptor(\.created, order: .reverse),
-      ])
+      ]
+    )
   }
 }
 
