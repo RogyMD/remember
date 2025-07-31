60679a0f60c7a72ba02c8e13a61348667d7fe8ae
Igor Bidiniuc <igorbidiniuc@gmail.com>
2025-07-31 01:03:03 +0100

    add text recognizer

--- RememberCore/Sources/TextRecognizerClient/TextRecognizerClient.swift
+++ RememberCore/Sources/TextRecognizerClient/TextRecognizerClient.swift
@@ -1,25 +1,97 @@
 import Dependencies
 import DependenciesMacros
+import UIKit
+import NaturalLanguage
 
-@DependencyClient
-public struct <#Example#>: Sendable {
-  @DependencyEndpoint
-  public var <#api#>: @Sendable (<#params#>) <#async#> <#throws#> -> <#Void#>
+public struct RecognizedTextFrame {
+  public var text: String
+  public var frame: CGRect
 }
 
-extension DependencyValues {
-  public var <#Example#>: <#Example#> {
-    get { self[<#Example#>.self] }
-    set { self[<#Example#>.self] = newValue }
+extension TextRecognizerClient {
+  public struct Result {
+    public let text: String
+    public let textFrames: [RecognizedTextFrame]
   }
 }
 
-extension <#Example#>: TestDependencyKey {
+@DependencyClient
+public struct TextRecognizerClient: Sendable {
+  @DependencyEndpoint
+  public var recognizeTextInImage: @Sendable (UIImage) async throws -> Result
+}
+
+extension DependencyValues {
+  public var textRecognizer: TextRecognizerClient {
+    get { self[TextRecognizerClient.self] }
+    set { self[TextRecognizerClient.self] = newValue }
+  }
+}
+
+extension TextRecognizerClient: TestDependencyKey {
   public static let testValue = Self()
 }
 
 // MARK: - Live
 
-extension <#Example#>: DependencyKey {
-  public static let liveValue: Self = Self(<#api#>: <#value#>)
+import Vision
+
+extension TextRecognizerClient: DependencyKey {
+  public static let liveValue: Self = Self(recognizeTextInImage: { image in
+    guard let cgImage = image.cgImage else {
+      return .init(text: "", textFrames: [])
+    }
+
+    let request = VNRecognizeTextRequest()
+    request.recognitionLevel = .accurate
+    request.usesLanguageCorrection = true
+
+    let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+    try handler.perform([request])
+
+    guard let observations = request.results else {
+      return .init(text: "", textFrames: [])
+    }
+
+    let screenScale = await MainActor.run {
+      UIScreen.main.scale
+    }
+
+    var allText = ""
+    let frames: [RecognizedTextFrame] = observations.flatMap { observation -> [RecognizedTextFrame] in
+      guard let topCandidate = observation.topCandidates(1).first else { return [] }
+      let fullText = topCandidate.string
+      let cleanedText = fullText.replacingOccurrences(
+        of: "[^\\p{L}\\p{N}\\s]",
+        with: "",
+        options: .regularExpression
+      )
+      allText += cleanedText + " "
+
+      var result: [RecognizedTextFrame] = []
+
+      let tagger = NLTagger(tagSchemes: [.lexicalClass])
+      tagger.string = fullText
+
+      tagger.enumerateTags(in: fullText.startIndex..<fullText.endIndex, unit: .word, scheme: .lexicalClass) { tag, tokenRange in
+        guard let tag = tag, (tag == .noun || tag == .organizationName) else { return true }
+
+        let word = String(fullText[tokenRange])
+        let rect = VNImageRectForNormalizedRect(observation.boundingBox, cgImage.width, cgImage.height)
+        let convertedRect = CGRect(
+          x: rect.origin.x / screenScale,
+          y: (CGFloat(cgImage.height) - rect.origin.y - rect.height) / screenScale,
+          width: rect.width / screenScale,
+          height: rect.height / screenScale
+        )
+
+        result.append(RecognizedTextFrame(text: word, frame: convertedRect))
+        return true
+      }
+
+      return result
+    }
+
+    return .init(text: allText.trimmingCharacters(in: .whitespacesAndNewlines), textFrames: frames)
+  })
 }
